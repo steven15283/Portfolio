ab9c0a7f0c8092d9af8e7e48d7e9b8ef2654d1fc
steven15283 <stevenguo15283@gmail.com>
2022-10-29 01:55:53 -0400

    removing DrawSVGPlugin

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,17 @@
+import LogoS from '../../../assets/images/logo-s.png'
+import './index.scss'
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img
+        className="solid-logo"
+        src={LogoS}
+        alt="S"
+      />
+
+    </div>
+  )
+}
+
+export default Logo
